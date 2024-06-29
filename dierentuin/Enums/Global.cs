5e455d978021c57194193b2e4b566005baa2603e
Pluers <S1204255@student.windesheim.nl>
2024-06-29 17:23:44 +0200

    move the enum animalsize

--- dierentuin/Enums/Global.cs
+++ dierentuin/Enums/Global.cs
@@ -6,15 +6,4 @@ namespace dierentuin.Enums
         Medium,
         High
     }
-
-    // To access the AnimalSize enum in both the api and the model
-    public enum AnimalSize
-    {
-        Microscopic,
-        VerySmall,
-        Small,
-        Medium = 5,
-        Large = 10,
-        VeryLarge = 20
-    }
 }
